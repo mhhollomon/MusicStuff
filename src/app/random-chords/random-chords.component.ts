538b4ffe5ed385d1d29fce463c74061c497017a1
Mark Hollomon <mhhollomon@gmail.com>
2022-12-31 16:10:43 -0500

    Tweak the midi and playback tones generated.

--- src/app/random-chords/random-chords.component.ts
+++ src/app/random-chords/random-chords.component.ts
@@ -6,6 +6,10 @@ import { RandomChordService, Chord, ChordType } from '../random-chord.service';
 import { Note, Scale, ScaleType } from '../key';
 import { AudioService } from '../audio.service';
 
+const octavePlacement : any = {
+  'C' : 0, 'D' : 1, 'E' : 2, 'F' : 3, 'G' : 4, 'A' : 5, 'B' : 6 
+}
+
 @Component({
   selector: 'app-random-chords',
   templateUrl: './random-chords.component.html',
@@ -88,16 +92,24 @@ export class RandomChordsComponent {
   async play_chord(chord : Chord) {
 
     let tones : string[] = [];
-    for (let i = 0; i < chord.chordTones.length; ++i ) {
-      if (i == 0) {
-        tones.push(chord.chordTones[i].note() + '3');
-      //} else if (i == 3 && chord.chordTones[i].note() < chord.chordTones[i-1].note()) {
-      //  tones.push(chord.chordTones[i].note() + '5');
-      } else {
-        tones.push(chord.chordTones[i].note() + '4');
-      }
-    }
+    let octave = 3;
+    let last : number = -1;
+    let isBassNote = true;
 
+    for (let c of chord.chordTones) {
+      if (octavePlacement[c.noteClass] < last) {
+        octave += 1;
+      }
+      tones.push(c.note() + octave);
+      if (isBassNote) {
+        octave += 1;
+        isBassNote = false;
+      } else {
+        last = octavePlacement[c.noteClass];
+      }
+
+    }
+  
     this.audioService.play_chord(tones);
 
   }
@@ -110,21 +122,43 @@ export class RandomChordsComponent {
 
     for (let c of this.chords) {
 
+      // Want to place the bass note "down an octave"
+      let octave = 3;
+      let last : number = -1;
+      let isBassNote = true;
+  
       let options : Midiwriter.Options = {sequential: false, duration : '1', pitch : []}
-      let pitches : string[]  = [];
 
-      for (let i = 0; i < c.chordTones.length; ++i ) {
-        if (i == 0) {
-          (options.pitch as unknown as string[]).push(c.chordTones[i].note() + '2');
-        //} else if (i == 3 && chord.chordTones[i].note() < chord.chordTones[i-1].note()) {
-        //  tones.push(chord.chordTones[i].note() + '5');
-        } else {
-          (options.pitch as unknown as string[]).push(c.chordTones[i].note() + '3');
+      for (let n of c.chordTones) {
+        if (octavePlacement[n.noteClass] < last) {
+          octave += 1;
         }
+        (options.pitch as unknown as string[]).push(n.note() + octave );
+        if (isBassNote) {
+          octave += 1;
+          isBassNote = false;
+        } else {
+          last = octavePlacement[n.noteClass];
+        }
+  
       }
 
       track.addEvent(new Midiwriter.NoteEvent(options))
     }
+
+    let octave = ['G', 'A', 'B'].includes(this.scale_notes[0].noteClass) ? 3 : 4;
+    let last : number = -1;
+    let scale_options : Midiwriter.Options = {sequential: true, duration : '4', pitch : []}
+    for (let n of this.scale_notes) {
+
+      if (octavePlacement[n.noteClass] < last) {
+        octave += 1;
+      }
+      (scale_options.pitch as unknown as string[]).push(n.note() + octave );
+      last = octavePlacement[n.noteClass];
+    }
+
+    track.addEvent(new Midiwriter.NoteEvent(scale_options))
 
 
     let midi_writer = new Midiwriter.Writer(track);
