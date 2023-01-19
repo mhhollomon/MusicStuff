7eb81d214e666a44c096635680088b8b381009df
Mark Hollomon <mhhollomon@gmail.com>
2023-01-19 07:21:19 -0500

    clean up random

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { CompositionIdeaComponent } from './composition-idea/composition-idea.component';
-import { RandomChordsComponent } from './random-chords/random-chords.component';
 import { MusicResourcesComponent } from './music-resources/music-resources.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BassScalesComponent } from './bass-scales/bass-scales.component';
@@ -12,7 +11,6 @@ const routes: Routes = [
     { path : '',   redirectTo : 'home', pathMatch: 'full' },
     { path : 'home',             component : HomeComponent },
     { path : 'composition-idea', component : CompositionIdeaComponent },
-    { path : 'random-chords',    component : RandomChordsComponent },
     { path : 'music-resources',  component : MusicResourcesComponent },
     { path : 'bass-scales',      component : BassScalesComponent },
 
